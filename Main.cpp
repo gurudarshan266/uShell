6b3031e3ae5eeb2ba6e60d9f5551742f2367d933
Guru Darshan Pollepalli Manohara <gpollep@ncsu.edu>
2016-10-19 11:08:52 -0700

    Added support for File redirection

--- Main.cpp
+++ Main.cpp
@@ -13,17 +13,30 @@ extern "C" {
 #include <sys/wait.h>
 #include <errno.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "parse.h"
 
 #ifdef __cplusplus
 }
 #endif
 
+#define DISP_ERR cout<<strerror(errno)<<endl;
+#define IN 0
+#define OUT 1
+#define ERR 2
+
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 void HandleExecutable(Cmd&);
 void HandleSetEnv(Cmd&);
 void HandleUnsetEnv(Cmd&);
 void HandleLogout(Cmd&);
 void HandleCd(Cmd&);
+
+void ManageIO(Cmd&);
+void ManagePipedCmd(Cmd&,int*,int*);
+
 
 void prCmd(Cmd c)
 {
@@ -65,7 +78,6 @@ void prCmd(Cmd c)
       printf("\b]");
     }
     putchar('\n');
-
 
 
     if ( !strcmp(c->args[0], "end") )
@@ -154,6 +166,64 @@ void HandleCd(Cmd& c)
 		cout<<strerror(errno)<<endl;
 }
 
+
+void ManageIO(Cmd& c)
+{
+	int fd[2];
+
+	if(c->in == Tin)//If input from a file
+	{
+		fd[IN] = open(c->infile,O_RDONLY);
+		if(fd[IN] < 0)
+		{
+			DISP_ERR
+			exit(-1);
+		}
+
+		dup2(fd[IN], IN);//Make the new fd, the stdin for this process
+	}
+
+	if(c->out == Tout || c->out == Tapp || c->out == ToutErr || c->out == TappErr)//If output to a file
+	{
+		int flags= O_WRONLY | O_CREAT;
+		flags |= ( (c->out==Tapp)? O_APPEND : 0 );
+
+		int mode = S_IRUSR | S_IWUSR;
+
+		fd[OUT] = open(c->outfile,flags, mode);
+
+		if(fd[OUT] < 0)
+		{
+			DISP_ERR
+			exit(-1);
+		}
+
+		dup2(fd[OUT],OUT);//Make the new fd, the stdout for this process
+
+		if(c->out == ToutErr || c->out == TappErr)
+		{
+			dup2(fd[OUT],ERR);
+		}
+	}
+}
+
+void ManagePipedCmd(Cmd& c, int* prevPipe, int* nextPipe)
+{
+	ManageIO(c);
+
+	if(c->in == Tpipe)
+	{
+		close(prevPipe[OUT]);
+		dup2(prevPipe[IN],IN);
+	}
+
+	if(c->out == Tpipe || c->out == TpipeErr)
+	{
+
+	}
+
+}
+
 /*
  * Look in absolute or relative path for the executable
  * If not found, search in PATH for the executable
@@ -166,7 +236,9 @@ void HandleExecutable(Cmd& c)
 	// Child
 	if(cpid == 0)
 	{
-		execvp(c->args[0],c->args);
+		ManageIO(c);
+
+		int res = execvp(c->args[0],c->args);
 		cout<<c->args[0]<<": command not found"<<endl;
 		exit(0);
 	}
@@ -183,11 +255,14 @@ int main(int argc, char *argv[])
   Pipe p;
   const char *host = getenv("USER");
 
+  char cwd[1024] = "";
+
   while ( 1 ) {
-    printf("%s%% ", host);
-    p = parse();
-    prPipe(p);
-    freePipe(p);
+	getcwd(cwd, sizeof(cwd));
+	printf("%s: "ANSI_COLOR_CYAN"%s "ANSI_COLOR_RESET"%% ", host, cwd);
+	p = parse();
+	prPipe(p);
+	freePipe(p);
   }
 }
 
